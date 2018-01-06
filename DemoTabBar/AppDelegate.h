701a45347efc425102ae89b9207ae1250e03ff76
Rajesh Karmaker <rajeshkarmaker0@gmail.com>
2018-01-06 18:02:28 +0600

    Initial Commit

---
+++ DemoTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoTabBar
+//
+//  Created by Rajesh's MacBook Pro  on 1/6/18.
+//  Copyright © 2018 MacBook Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DemoTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoTabBar
+//
+//  Created by Rajesh's MacBook Pro  on 1/6/18.
+//  Copyright © 2018 MacBook Pro Retina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
